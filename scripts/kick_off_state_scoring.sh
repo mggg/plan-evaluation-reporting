97b13ccbbf1edb5b058fc9fe7c9959a3494129ca
jenni-niels <jenniniels.matthews@gmail.com>
2021-09-30 17:12:53 -0400

    Add cluster bash scripts.

---
+++ scripts/kick_off_state_scoring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+state = $1
+echo $state
+
+for map in  "congress" "state_senate" "state_house"
+do
+    echo $map
+    for county_aware in 0 1
+    do
+        [ $county_aware == 1 ] && county_aware_str="county_aware" || county_aware_str="neutral"
+        echo $county_aware_str
+        sbatch -o logs/$state.$map.$county_aware_str.score_ensemble.%x.%A_%a.txt score_chain.slurm $state $map $county_aware
+        sleep 1
+    done
+done
