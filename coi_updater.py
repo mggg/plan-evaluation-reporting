cb9000cefc87a82ef28636e65a4019b1f166951e
Max Fan <root@max.fan>
2021-10-16 16:35:47 -0500

    Amend impl to take the pairwise sum

--- coi_updater.py
+++ coi_updater.py
@@ -5,10 +5,11 @@ def coi_updater(cluster_pops, cluster_sizes, totpop):
         district_cluster_idealized = [x / ideal_pop for x in district_cluster_pops]
         district_cluster_normalized = [x / cluster_sizes[cluster] for x, cluster in zip(district_cluster_pops, cluster_pops)]
 
-        idealized_count = sum([1 for x in district_cluster_idealized if x > 0.75])
-        normalized_count = sum([1 for x in district_cluster_normalized if x > 0.75])
+        # print(district_cluster_idealized)
+        # print(district_cluster_normalized, sum(district_cluster_normalized))
 
-        print(idealized_count, normalized_count)
-        return min(idealized_count, normalized_count)
+        score = sum([1 for x, y in zip(district_cluster_idealized, district_cluster_normalized) if x > 0.75 or y > 0.75])
+        print(score)
+        return score
 
     return updater
