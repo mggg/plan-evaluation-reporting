daff8f53a562c2239333ecaee165d5a84899143f
ccthegreat313 <crichardson1399@gmail.com>
2021-12-22 13:26:52 -0500

    Code cleanup

--- basic_stats/block_to_vtd_mapping.py
+++ basic_stats/block_to_vtd_mapping.py
@@ -5,58 +5,68 @@ import glob
 import tqdm
 
 def block_to_vtd_csv(vtd_shp, block_shp, state_blocks_to_vtds, csv_directory):
-	'''
-	Uses a json file mapping blocks to vtds to do a conversion from a block assignment file to a vtd assignment file. 
-	If a vtd is assigned to multiple districts based on this mapping, then it is put in the district with the minimum
-	population.
+    '''
+    Outputs vtd assignments for plans initially on blocks. Assigns split vtds with to the district that contains
+    the most of its population.
+
+    Args:
 	vtd_shp: Shapefile for the state on vtds
 	block_shp: Shapefile for the state on blocks
 	state_blocks_to_vtds: Json file outlining the mapping for vtds to blocks. VTD GEOIDs are keys, blocks are values
 	csv_directory: Directory containing plans as block assignment files
-	'''
-	json_f = open(state_blocks_to_vtds)
-	vtds_to_blocks = json.load(json_f)
+    '''
 
-	files = glob.glob(csv_directory + "/*.csv")
-	state_vtd = gpd.read_file(vtd_shp)
-	state_block = gpd.read_file(block_shp, dtype={"GEOID20": "str"})
+    json_f = open(state_blocks_to_vtds)
+    vtds_to_blocks = json.load(json_f)
 
-	for file in files:
-	 plan = pd.read_csv(file, dtype={"GEOID20": 'str', "assignment":int})
-	 print(file.split("/")[-1].split(".csv")[0])
-	 plan = plan.merge(state_block, on="GEOID20")
+    files = glob.glob(csv_directory + "/*.csv")
+    state_vtd = gpd.read_file(vtd_shp)
+    state_block = gpd.read_file(block_shp, dtype={"GEOID20": "str"})
+	
+    pop_key = [key if "TOTPOP" in key for key in state_block.columns]
+
+    for file in files:
+        plan = pd.read_csv(file, dtype={"GEOID20": "str", "assignment":int})
+	
+        print(file.split("/")[-1].split(".csv")[0])
+        plan = plan.merge(state_block, on="GEOID20")
+        vtds = []
+        vtd_assignment = [] 
+
+        for vtd in tqdm.tqdm(vtds_to_blocks.keys()):
+            blocks = [str(x) for x in vtds_to_blocks[vtd]]
+            vtd_assignment.extend(plan["assignment"][plan["GEOID20"].isin(blocks)].values
+            vtds.extend(len(plan["assignment"][plan["GEOID20"].isin(blocks)].values) * [vtd])
 	 
-	 vtds = []
-	 vtd_assignment = [] 
-	 for vtd in tqdm.tqdm(vtds_to_blocks.keys()):
-	   blocks = vtds_to_blocks[vtd]
-	   vtd_assignment.extend(plan["assignment"][plan["GEOID20"].isin(blocks)].values)
-	   vtds.extend(len(blocks) * [vtd])
+        vtd_df = pd.DataFrame(columns =  ["GEOID20", "assignment"])
+        vtd_df["GEOID20"] = vtds
+        vtd_df["assignment"] = vtd_assignment
+	 
+        vtd_df = vtd_df.drop_duplicates()
+	 
+        geoids = vtd_df["GEOID20"]
+        vtd_dups = vtd_df[geoids.isin(geoids[geoids.duplicated()])]
+        tot_reassigned_pop = []
+    
+        for vtd in tqdm.tqdm(vtd_dups.GEOID20.unique()):
+            assignments = vtd_dups["assignment"][vtd_dups["GEOID20"] == vtd].values
+            max_pop = 0
+            max_district = 0
+            pop_list = []
 
-	 assignments = {"GEOID20" : vtds, "assignment": vtd_assignment}
-	 vtd_df = pd.DataFrame(columns =  ["GEOID20", "assignment"])
-	 vtd_df["GEOID20"] = vtds
-	 vtd_df["assignment"] = vtd_assignment
+            for district in assignments:
+                pop_list.append(sum(plan[pop_key][(plan["assignment"] == district) & (plan["GEOID20"].isin(vtds_to_blocks[vtd]))]))
 
-	 vtd_df = vtd_df.drop_duplicates()
-	 geoids = vtd_df["GEOID20"]
-	 vtd_dups = vtd_df[geoids.isin(geoids[geoids.duplicated()])]
-	 for vtd in vtd_dups.GEOID20.unique():
-	  assignments = vtd_dups["assignment"][vtd_dups["GEOID20"] == vtd].values
-	  max_pop = 0
-	  max_district = 0
-	  pop_list = []
-	  for district in assignments:
-	    pop =  sum(plan["TOTPOP20"][(plan["assignment"] == district) & (plan["GEOID20"].isin(vtds_to_blocks[vtd]))])
-	    pop_list.append(pop)
-	    if pop >= max_pop:
-	      max_pop = pop
-	      max_district = district
-	  vtd_df = vtd_df[vtd_df.GEOID20 != vtd]
-	  vtd_df = vtd_df.append({"GEOID20": vtd, "assignment": max_district}, ignore_index = True)
+                if pop_list[-1] >= max_pop:
+                    max_pop = pop
+                    max_district = district
+	  
+                vtd_df = vtd_df[vtd_df.GEOID20 != vtd]
+                vtd_df = vtd_df.append({"GEOID20": vtd, "assignment": max_district}, ignore_index = True)
  
-	 vtd_df = vtd_df[["GEOID20", "assignment"]]
-	 fname = file.split(".csv")[0] + "_vtd.csv"
-	 vtd_df.to_csv(fname, index=False)
+        vtd_df = vtd_df[["GEOID20", "assignment"]]
+        fname = file.split(".csv")[0] + "_vtd.csv"
+        vtd_df.to_csv(fname, index=False)
+
 
 
--- basic_stats/polsby_per_dist.py
+++ basic_stats/polsby_per_dist.py
@@ -3,15 +3,24 @@ import geopandas as gpd
 from math import pi
 
 def polsby_per_dist(plan_on_blocks, block_shp):
-  plan = pd.read_file(plan_on_blocks, dtype={"GEOID20":"str"})
-  state_shp = gpd.read_file(block_shp, dtype={"GEOID20":"str"})
-  plan = plan.merge(state_shp, on = "GEOID20")
-  
-  dissolved_plan = plan.dissolve(by="assignment")
+    """
+    Takes a plan on blocks, dissolves the geometries down to the district level, and computes the polsby popper score.
 
-  
-  return [(row.assignment, (4*pi*row.geometry.area)/(row.geometry.length**2)) for idx, row in dissolved_plan.iterrows()]
-  
+    Args:
+        plan_on_blocks: Assignment csv for a plan on blocks. Assumes that columns are named "GEOID20" and "assignment".
+        block_shp: Shapefile for the state on blocks.
 
+    Returns: 
+         Dictionary mapping each district in the plan to its polsby popper score.
+    """
+    plan = pd.read_csv(plan_on_blocks, dtype={"GEOID20":"str"})
+    state_shp = gpd.read_file(block_shp, dtype={"GEOID20":"str"})
+  
+    plan = plan.merge(state_shp, on = "GEOID20") 
+    dissolved_plan = plan.dissolve(by="assignment")
+ 
+    polsby_dict = {row.assignment: (4*pi*row.geometry.area)/(row.geometry.length**2)) for idx, row in dissolved_plan.iterrows()}
+    return polsby_dict
+ 
 
 
